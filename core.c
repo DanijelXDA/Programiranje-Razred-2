e8dca5f216d84f06c5bd9c67d05de21126a79785
danijelxda <danijelj2001.xda@gmail.com>
2018-08-28 21:12:17 +0200

    Add description of some parts of code

--- core.c
+++ core.c
@@ -2,12 +2,15 @@
 
 int main(void)
 {
+    /* Test scenario za biblioteku formiranjeNiza.h */
     int n;
     int niz[50]; // Jednodimenzionalni niz
 
     brojElemenataNiza(&n); // Poziv nase funkcije
     formiranjeNiza(niz, n); // Unos elemenata niza
     ispisElemenataNiza(niz, n); // Ispis elemenata niza
+    /* Kraj test scenarija za biblioteku formiranjeNiza.h */
+
 
 
     return 0;
