97c159dc921e01dbaaf45e1d31a739a630033c0c
danijelxda <danijelj2001.xda@gmail.com>
2018-08-28 21:17:11 +0200

    Import previously made header

--- libs/1DNizovi/pretrazivanjeNiza.h
+++ libs/1DNizovi/pretrazivanjeNiza.h
@@ -1,6 +1,6 @@
 #ifndef PRETRAZIVANJENIZA_H_INCLUDED
 #define PRETRAZIVANJENIZA_H_INCLUDED
 
-
+#include "formiranjeNiza.h" // Biblioteka potrebna da formiramo niz
 
 #endif // PRETRAZIVANJENIZA_H_INCLUDED
