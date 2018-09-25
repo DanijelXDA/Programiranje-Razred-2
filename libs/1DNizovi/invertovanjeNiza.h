224ae027dcdf47903ab7144a3ec19f580fcf6126
danijelxda <danijelj2001.xda@gmail.com>
2018-09-25 21:16:58 +0200

    Include custom header

--- libs/1DNizovi/invertovanjeNiza.h
+++ libs/1DNizovi/invertovanjeNiza.h
@@ -1,6 +1,6 @@
 #ifndef INVERTOVANJENIZA_H_INCLUDED
 #define INVERTOVANJENIZA_H_INCLUDED
 
-
+#include "formiranjeNiza.h" // Biblioteka potrebna da formiramo niz
 
 #endif // INVERTOVANJENIZA_H_INCLUDED
