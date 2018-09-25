bed5293f9212badad058ce69456ab8520817df9c
danijelxda <danijelj2001.xda@gmail.com>
2018-09-25 21:17:35 +0200

    Include custom header

--- libs/1DNizovi/rotiranjeNizaKPuta.h
+++ libs/1DNizovi/rotiranjeNizaKPuta.h
@@ -1,6 +1,6 @@
 #ifndef ROTIRANJENIZAKPUTA_H_INCLUDED
 #define ROTIRANJENIZAKPUTA_H_INCLUDED
 
-
+#include "formiranjeNiza.h" // Biblioteka potrebna da formiramo niz
 
 #endif // ROTIRANJENIZAKPUTA_H_INCLUDED
