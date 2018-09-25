58ef7eeb7295ab4fc19b6117e83eb0d438d4fe40
danijelxda <danijelj2001.xda@gmail.com>
2018-09-25 21:17:18 +0200

    Include custom header

--- libs/1DNizovi/iznacivanjeElementaIzNiza.h
+++ libs/1DNizovi/iznacivanjeElementaIzNiza.h
@@ -1,6 +1,6 @@
 #ifndef IZNACIVANJEELEMENTAIZNIZA_H_INCLUDED
 #define IZNACIVANJEELEMENTAIZNIZA_H_INCLUDED
 
-
+#include "formiranjeNiza.h" // Biblioteka potrebna da formiramo niz
 
 #endif // IZNACIVANJEELEMENTAIZNIZA_H_INCLUDED
