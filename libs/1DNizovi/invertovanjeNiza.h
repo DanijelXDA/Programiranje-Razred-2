23b122cb95b9ef2281b93382379df1d7119c87c7
Danijel Jovanović <36568690+DanijelXDA@users.noreply.github.com>
2018-11-03 13:57:16 +0100

    Bring v2.55.4

--- libs/1DNizovi/invertovanjeNiza.h
+++ libs/1DNizovi/invertovanjeNiza.h
@@ -3,4 +3,15 @@
 
 #include "formiranjeNiza.h" // Biblioteka potrebna da formiramo niz
 
+void invertovanjeElemenata(int niz[], int n) {
+    int i, j;
+    int temp;
+
+    // Invertovanje elemenata niza
+    for(i = 0; i < n / 2; i++) {
+        for(j = n - 1; j > n / 2; j++) {
+
+        }
+    }
+}
 #endif // INVERTOVANJENIZA_H_INCLUDED
